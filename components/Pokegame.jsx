adc920989162d7f2df512b2b472b6dd9e41299e3
deanna <42.dpiedra@gmail.com>
2022-04-01 15:34:41 +0200

    fixed instructions

--- components/Pokegame.jsx
+++ components/Pokegame.jsx
@@ -37,7 +37,6 @@ function Pokegame() {
     <PokegameDiv>
       <PokegameH1>Welcome to the Pokedex Challenge</PokegameH1>
       <PokegameP>Two teams will compete and the team with the highest total Exp wins!</PokegameP>
-      <PokegameP>Your team is the one on top while the computers is the one on the bottom.</PokegameP>
       <Pokedex pokemons={hand1}
                exp={totalExp1}
                isWinner = {totalExp1 > totalExp2}
